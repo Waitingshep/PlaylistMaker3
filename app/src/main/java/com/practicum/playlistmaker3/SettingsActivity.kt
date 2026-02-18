9d27fda2a761ad6644771e801015084d464b494a
Vikhryanova Tatyana <vikhry-tatyana@yandex.ru>
2026-02-19 01:56:30 +0700

    Вторая домашка по плейлисту

--- app/src/main/java/com/practicum/playlistmaker3/SettingsActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker3/SettingsActivity.kt
@@ -2,12 +2,10 @@ package com.practicum.playlistmaker3
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
-import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import android.widget.ImageButton
+import android.widget.TextView
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
 
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,6 +16,40 @@ class SettingsActivity : AppCompatActivity() {
 
         backButton.setOnClickListener {
             finish()
+        }
+
+        val shareTextView = findViewById<TextView>(R.id.share_app)
+
+        shareTextView.setOnClickListener {
+            val shareIntent = Intent(Intent.ACTION_SEND).apply {
+                type = "text/plain"
+                putExtra(Intent.EXTRA_TEXT, getString(R.string.course_url))
+            }
+
+            val chooser = Intent.createChooser(shareIntent, "Поделиться через")
+            startActivity(chooser)
+        }
+
+        val supportTextView = findViewById<TextView>(R.id.message_support)
+
+        supportTextView.setOnClickListener {
+            val message = getString(R.string.support_message)
+            val subject = getString(R.string.support_subject)
+
+            val supportIntent = Intent(Intent.ACTION_SENDTO)
+            supportIntent.data = Uri.parse("mailto:")
+            supportIntent.putExtra(Intent.EXTRA_EMAIL, arrayOf(getString(R.string.support_email)))
+            supportIntent.putExtra(Intent.EXTRA_TEXT, message)
+            supportIntent.putExtra(Intent.EXTRA_SUBJECT, subject)
+            startActivity(supportIntent)
+        }
+
+        val agreementTextView = findViewById<TextView>(R.id.agreement)
+
+        agreementTextView.setOnClickListener {
+            val agreementUrl = getString(R.string.agreement_url)
+            val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse(agreementUrl))
+            startActivity(browserIntent)
         }
     }
 }
